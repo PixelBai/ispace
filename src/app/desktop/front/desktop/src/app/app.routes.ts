0d0ac78814b8118cc24a4dd940d457c6cf4d1095
baialei <850568054@qq.com>
2024-07-22 09:13:43 +0800

    init 架构

--- src/app/desktop/front/desktop/src/app/app.routes.ts
+++ src/app/desktop/front/desktop/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { IndexComponent } from './page/index/index.component';
+import { LoginCanActivate } from './system/login-gurd';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path:'',redirectTo:"index",pathMatch:'full'},      
+    { path:'index',component:IndexComponent,canActivate: [LoginCanActivate] },
+];
