0e083b6dfe6cee83226fb281ffa41ab49c4b085f
baialei <850568054@qq.com>
2024-07-30 23:49:34 +0800

    init de

---
+++ src/cmnComp/DriveEngin/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');  
+  
+module.exports = {  
+  entry: './src/index.ts',  
+  module: {  
+    rules: [  
+      {  
+        test: /\.tsx?$/,  
+        use: 'ts-loader',  
+        exclude: /node_modules/,  
+      },  
+    ],  
+  },  
+  resolve: {  
+    extensions: ['.ts', '.js'],  
+  },  
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'ispace_de', // 导出为库
+    libraryTarget: 'umd', // 通用模块定义
+    globalObject: 'this',
+  },
+  mode: 'production', // 或 'development' 根据需要
+  watch: true, 
+};
