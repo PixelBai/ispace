98c1d4a562b50581a6a0d67e14e03499c2ecb6ce
alei <850568054@qq.com>
2024-08-08 11:23:41 +0800

    init page

--- src/app/texteditor/front/textEditor/src/app/app.config.ts
+++ src/app/texteditor/front/textEditor/src/app/app.config.ts
@@ -3,7 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), {  
+    provide: LocationStrategy,  
+    useClass: HashLocationStrategy  
+  }  ,provideRouter(routes), provideAnimationsAsync()]
 };
+
+ 
---
+++ src/app/texteditor/front/textEditor/src/app/system/login-gurd.ts
@@ -0,0 +1,24 @@
+
+import { ActivatedRouteSnapshot, RouterStateSnapshot, CanActivateFn } from "@angular/router";
+import { auth } from "ispace.core.main";
+
+export const LoginCanActivate: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+  return true;
+  return new Promise<boolean>((resolve) => {
+    auth.isLogin().subscribe({
+      next: (s) => {
+        if (!s.success) {
+          window.location.href = "/front/src/login.html?returnUrl=" + window.location.href;
+          resolve(false);
+          return;
+        }
+        resolve(true);
+      }, error: (e: any) => {
+        console.log(e);
+        window.location.href = "/front/src/login.html?returnUrl=" + window.location.href;
+        resolve(false);
+        return;
+      }
+    });
+  });
+};
