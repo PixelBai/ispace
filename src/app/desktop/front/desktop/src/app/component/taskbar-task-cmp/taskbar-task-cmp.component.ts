48a6c375b9078b2443495170c49a6d30968ff2c5
baialei <850568054@qq.com>
2024-09-11 22:34:03 +0800

    u

--- src/app/desktop/front/desktop/src/app/component/taskbar-task-cmp/taskbar-task-cmp.component.ts
+++ src/app/desktop/front/desktop/src/app/component/taskbar-task-cmp/taskbar-task-cmp.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, viewChild, ViewChild, ViewChildren } from '@angular/core';
 import { WindowsManagerService } from '../../service/windows-manager.service';
 import { CommonModule } from '@angular/common';
 import { CdkMenu, CdkMenuItem, CdkMenuModule, CdkMenuTrigger } from '@angular/cdk/menu';
 import { MatButtonModule } from '@angular/material/button';
-import {MatChipsModule} from '@angular/material/chips';
+import {MatChip, MatChipSet, MatChipsModule} from '@angular/material/chips';
 import { CdkDropList, CdkDrag, CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { WindowDto } from '../../dto/windowDto';
 import { setThrowInvalidWriteToSignalError } from '@angular/core/primitives/signals';
@@ -15,13 +15,14 @@ import { setThrowInvalidWriteToSignalError } from '@angular/core/primitives/sign
   styleUrl: './taskbar-task-cmp.component.sass'
 })
 export class TaskbarTaskCmpComponent {
- 
+  
   windowDtos:WindowDto[] = [] 
   
-  constructor(public wsm: WindowsManagerService) { 
+  constructor(public wsm: WindowsManagerService,private renderer: Renderer2) { 
 
     this.wsm.windowDtos.subscribe(s => {
       this.windowDtos = s.slice().sort((a,b)=>a.taskbarSort-b.taskbarSort);
+      this.rendering();
     });
 
   }
@@ -50,6 +51,40 @@ export class TaskbarTaskCmpComponent {
     }
     this.windowDtos.sort((a,b)=>a.taskbarSort-b.taskbarSort);
   }
+
+  ngAfterViewInit() {
+
+    this.rendering();
+  }
+
+
+  @ViewChild('chipset') chipset?: any;
+  rendering_defaultWidth = 200;
+  rendering_width = this.rendering_defaultWidth;
+
+  rendering() { 
+    
+    if (this.windowDtos.length <= 0) {
+      return;
+    }
+
+    debugger;
+    // step 1: 获取当前最大宽度
+    if(this.chipset){
+       
+      let width = this.chipset._elementRef.nativeElement.offsetWidth;
+      let one_width = Math.floor(width/this.windowDtos.length);
+
+      if(one_width < this.rendering_defaultWidth){
+        this.rendering_width = one_width-10;
+      }
+      else{
+        this.rendering_width = this.rendering_defaultWidth;
+      }
+    }
+  }
   
+
+
 
 }
--- src/app/desktop/front/desktop/src/app/component/window-container-cmp/window-container-cmp.component.ts
+++ src/app/desktop/front/desktop/src/app/component/window-container-cmp/window-container-cmp.component.ts
@@ -20,7 +20,7 @@ export class WindowContainerCmpComponent {
   constructor(public wsm: WindowsManagerService,public sanitizer: DomSanitizer) {
  
     for (let i = 0; i < 5; i++) { 
-      this.wsm.open("新窗口"+i, "http://127.0.0.1/app/text-editor/#/home?path=Desktop%2F123.txt"); 
+      this.wsm.open("新窗口--------"+i, "http://127.0.0.1/app/text-editor/#/home?path=Desktop%2F123.txt"); 
     }
 
     this.wsm.windowDtos.subscribe(s => {
