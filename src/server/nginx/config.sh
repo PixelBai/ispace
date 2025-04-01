eb33439e9fe5ce13630da33e088163e9247f4b7b
baialei <850568054@qq.com>
2025-04-01 00:51:37 +0000

    1

--- src/server/nginx/config.sh
+++ src/server/nginx/config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 复制新的配置文件到 Nginx 配置目录
-sudo cp /root/code/ispace/src/server/nginx/config/nginx.conf /etc/nginx/nginx.conf
+sudo cp /code/ispace/src/server/nginx/config/nginx.conf /etc/nginx/nginx.conf
 if [ $? -ne 0 ]; then
     echo "Failed to copy nginx.conf"
     exit 1
